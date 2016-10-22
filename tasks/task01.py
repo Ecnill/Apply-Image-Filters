bc42bd8ba2c316a494c74064362beecc74546d0d
Ecnill <ecnill.j@gmail.com>
2016-10-22 12:53:41 +0200

    task 02: Ulam spiral

--- tasks/task01.py
+++ tasks/task01.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-# coding: utf-8
-# author: Ecnill
-# created: 15.10.2016
+# coding:   utf-8
+# author:   Ecnill
+# title:    Molecule
+# created:  15.10.2016
 
 """ Zahrejte si na molekulu:
 Vystartujte z náhodného místa konzole
@@ -10,8 +11,9 @@ podle (ideálního) zákona odrazu. """
 
 import os
 import time
-import random
 import sys
+from random import randint, choice
+from collections import namedtuple
 
 
 def clear_screen():
@@ -19,18 +21,14 @@ def clear_screen():
 
 
 def draw_border(length, height):
-    print('\033[34m' + "#" * length, end='')
+    print('\033[34m' + '#' * length, end='')
     for i in range(1, height - 2):
-        print('#', '{:^{spaces}}'.format("", spaces=length - 2), '#', sep='')
-    print("#" * length + '\033[0m', end='')
+        print('#', '{:^{spaces}}'.format('', spaces=length - 2), '#', sep='')
+    print('#' * length + '\033[0m', end='')
 
 
 def print_char_here(x, y, char):
-    x = int(x)
-    y = int(y)
-    horiz = str(x)
-    vert = str(y)
-    print("\033[" + vert + ";" + horiz + "f" + "\033[" + str(random.randint(31, 37)) + "m", char, "\033[0m")
+    print('\033[%s;%sf\033[%sm' % (int(y), int(x), randint(31, 37)), char, '\033[0m')
 
 
 def erase_chars(pos_x, pos_y):
@@ -46,32 +44,32 @@ def move(screen_size, pos, direction):
     pos[0] += direction[0]
     pos[1] += direction[1]
     draw_square(pos[0], pos[1])
-    if pos[0] == 2 or pos[0] == screen_size[0]:
+    if pos[0] == 2 or pos[0] == screen_size.length:
         direction[0] *= -1
-    if pos[1] == 3 or pos[1] == screen_size[1]:
+    if pos[1] == 3 or pos[1] == screen_size.height:
         direction[1] *= -1
     return [pos, direction]
 
 
-def render_loop(speed=0.3):
+def render_loop(speed=0.1):
     ts = os.get_terminal_size()
-    clear_screen()
-    os.system('setterm -cursor off')
     columns = ts.columns
     lines = ts.lines - 1
-    print('\033[31m' + 'BI-PYT hometask #01:' + '\033[34m' + ' Molekula.' + '\033[0m')
+    print('\033[31m' + 'BI-PYT hometask #01:' + '\033[34m' + ' Molekule.' + '\033[0m')
     draw_border(columns, lines)
     height = lines - 1
     length = columns - 3
-    screen_size = [length, height]
-    vector = [random.choice((-1, 1)), random.choice((-1, 1))]
-    square_pos = [random.randint(2, length), random.randint(3, height)]  # x, y
+    Screen_Size = namedtuple('Screen_Size', ['length', 'height'])
+    vector = [choice((-1, 1)), choice((-1, 1))]
+    square_pos = [randint(2, length), randint(3, height)]     # x, y
     while True:
-        square_pos, vector = move(screen_size, square_pos, vector)
+        square_pos, vector = move(Screen_Size(length, height), square_pos, vector)
         time.sleep(speed)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    clear_screen()
+    os.system('setterm -cursor off')
     try:
         arg_len = len(sys.argv)
         if arg_len > 1:
--- tasks/task02.py
+++ tasks/task02.py
@@ -1,5 +1,44 @@
 #!/usr/bin/env python3
-# coding: utf-8
-# author: Ecnill
+# coding:   utf-8
+# author:   Ecnill
+# title:    Ulam spiral
+# created:  22.10.16
 
-print("hello!")
+import os
+from math import sqrt, floor
+
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def is_prime(a):
+    if a < 2:
+        return False
+    for i in range(2, int(sqrt(a)) + 1):
+        if a % i == 0:
+            return False
+    return True
+
+
+def spiral(n, x, y):
+    d, y, x = 0, y - n//2, x - (n - 1)//2
+    l = 2 * max(abs(x), abs(y))
+    d = (l*3 + x + y) if y >= x else (l - x - y)
+    return (l - 1)**2 + d
+
+
+def set_numb_view(numb):
+    view = '\033[47m' if is_prime(numb) else '\033[0m'
+    return view + '{:{prec}d}'.format(numb, prec=4)
+
+
+if __name__ == '__main__':
+    clear_screen()
+    print('\033[31m' + 'BI-PYT hometask #02:' + '\033[34m' + ' Ulam spiral.' + '\033[0m')
+
+    n = floor(os.get_terminal_size().columns / 5)
+    for y in range(n):
+        for x in range(n):
+            print(set_numb_view(spiral(n, x, y)), end=' ')
+        print()
