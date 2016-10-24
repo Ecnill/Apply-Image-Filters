b087c52f55cf78818da8926c652d57b841cd9493
Ecnill <ecnill.j@gmail.com>
2016-10-24 11:01:22 +0200

    edit methods names

--- tasks/task02.py
+++ tasks/task02.py
@@ -21,24 +21,28 @@ def is_prime(a):
     return True
 
 
-def spiral(n, x, y):
+def set_numb_view(numb):
+    view = '\033[47m' if is_prime(numb) else '\033[0m'
+    return view + '{:{prec}d}'.format(numb, prec=4)
+
+
+def spiral_item(n, x, y):
     d, y, x = 0, y - n//2, x - (n - 1)//2
     l = 2 * max(abs(x), abs(y))
     d = (l*3 + x + y) if y >= x else (l - x - y)
     return (l - 1)**2 + d
 
 
-def set_numb_view(numb):
-    view = '\033[47m' if is_prime(numb) else '\033[0m'
-    return view + '{:{prec}d}'.format(numb, prec=4)
-
+def show_spiral():
+    n = floor(os.get_terminal_size().columns / 5)
+    for y in range(n):
+        for x in range(n):
+            print(set_numb_view(spiral_item(n, x, y)), end=' ')
+        print()
 
 if __name__ == '__main__':
     clear_screen()
     print('\033[31m' + 'BI-PYT hometask #02:' + '\033[34m' + ' Ulam spiral.' + '\033[0m')
+    show_spiral()
 
-    n = floor(os.get_terminal_size().columns / 5)
-    for y in range(n):
-        for x in range(n):
-            print(set_numb_view(spiral(n, x, y)), end=' ')
-        print()
+
